dca122f6d04c6089318c15181009d61125c31269
niemix <aloe.niemi888@gmail.com>
2024-11-09 18:20:10 +0300

    Add files via upload

---
+++ lesson_1.py
@@ -0,0 +1,20 @@
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+def string_info(string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+def is_contains (string, list_to_search):
+    count_calls()
+    for i in list_to_search:
+        if string.upper() == i.upper():
+            return True
+    return False
+print(string_info('Unicorn'))
+print(string_info('sunflower'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(calls)
+
+
